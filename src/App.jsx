07ef210a16f981c4ac9719eb89737f7141e7e91f
Sandesh Bhusal <sandeshbhusal417@gmail.com>
2025-05-26 23:14:39 +0545

    fix: app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer";
-import AppRoutes from "./Routes/AppRoutes";
+import AppRoutes from "./Routes/appRoutes";
 
 const App = () => {
   return (
