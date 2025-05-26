7dd52fd64f45a2bd2da990df138d04e93d0823a6
Sandesh Bhusal <sandeshbhusal417@gmail.com>
2025-05-26 23:11:44 +0545

    fix2

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer";
-import AppRoutes from "./Routes/appRoutes";
+import AppRoutes from "./Routes/AppRoutes";
 
 const App = () => {
   return (
